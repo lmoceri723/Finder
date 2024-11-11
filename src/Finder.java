f4da8191f97e28f04690fae3e270ed2ab985e7df
lmoceri723 <landon.moceri@menloschool.org>
2024-11-10 21:38:17 -0800

    working on final version

--- src/Finder.java
+++ src/Finder.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Finder
@@ -17,24 +16,20 @@ public class Finder {
 
     public Finder() {}
 
-    // Pair class to store key-value pairs
-    // This is needed, as keys and values are needed to resolve collisions
     public class Pair {
         String key;
         String val;
 
-        Pair (String key, String val) {
+        Pair(String key, String val) {
             this.key = key;
             this.val = val;
         }
     }
 
     public class HashMap {
-        // 16553, which will cause the total size to be 16 B * 16443 = 263 KB
         private final int INITIAL_SIZE = 16553;
         private final int RADIX = 256;
-        private final double LOAD_FACTOR = 0.5;
-
+        private final double CAPACITY = 0.5;
 
         private int filledSize = 0;
         private int tableSize = INITIAL_SIZE;
@@ -45,26 +40,22 @@ public class Finder {
             table = new Pair[tableSize];
         }
 
-        // Hash function to hash a string using polynomial rolling hash
         public int hash(String key, int p) {
             int hash = 0;
             for (int i = 0; i < key.length(); i++) {
                 hash = (hash * RADIX + key.charAt(i)) % p;
             }
-            // Add p to ensure the hash is positive
             hash = (hash + p) % p;
             return hash;
         }
 
         public void resize() {
             tableSize *= 2;
-            filledSize = 0;
             Pair[] newTable = new Pair[tableSize];
 
             for (Pair pair : table) {
                 if (pair != null) {
-                    insertPrimitive(newTable, pair.key, pair.val);
-                    filledSize++;
+                    insertHelper(newTable, pair.key, pair.val);
                 }
             }
 
@@ -72,38 +63,28 @@ public class Finder {
         }
 
         public void insert(String key, String val) {
-            if (LOAD_FACTOR * 0.5 <= filledSize) {
+            if (filledSize >= CAPACITY * tableSize) {
                 resize();
             }
-            insertPrimitive(table, key, val);
+            insertHelper(table, key, val);
             filledSize++;
         }
 
-        public void insertPrimitive(Pair[] t, String key, String val) {
+        public void insertHelper(Pair[] t, String key, String val) {
             int hash = hash(key, tableSize);
-            if (t[hash] == null) {
-                t[hash] = new Pair(key, val);
-            } else {
-                int i = hash;
-                while (t[i] != null) {
-                    i++;
-                }
-                t[i] = new Pair(key, val);
+            while (t[hash] != null) {
+                hash = (hash + 1) % tableSize;
             }
+            t[hash] = new Pair(key, val);
         }
 
         public String query(String key) {
             int hash = hash(key, tableSize);
-            if (table[hash] != null && table[hash].key.equals(key)) {
-                return table[hash].val;
-            } else {
-                int i = hash;
-                while (table[i] != null && !table[i].key.equals(key)) {
-                    i++;
+            while (table[hash] != null) {
+                if (table[hash].key.equals(key)) {
+                    return table[hash].val;
                 }
-                if (table[i] != null) {
-                    return table[i].val;
-                }
+                hash = (hash + 1) % tableSize;
             }
             return INVALID;
         }
@@ -111,28 +92,19 @@ public class Finder {
 
     private HashMap map = new HashMap();
 
-    // This function should build a hash table from the given BufferedReader
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // Read the first line
         String line = br.readLine();
-
-        // Iterate over each subsequent line
         while (line != null) {
-            // Grab the specified columns to get the key and value
             String[] columns = line.split(",");
             String key = columns[keyCol];
             String val = columns[valCol];
-
-            // Insert the key-value pair into the hash table
             map.insert(key, val);
-
             line = br.readLine();
         }
         br.close();
     }
 
-    // Finds whether a value exists for a given key in the hash table
-    public String query(String key){
+    public String query(String key) {
         return map.query(key);
     }
 }
